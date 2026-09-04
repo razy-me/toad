f596771b2cdceaee1556aa54622d337def62623d
razy-me <florian8schuster@gmail.com>
2026-09-04 23:07:54 +0200

    chore: remove scratch/dev folders and legacy request/test planning docs, update .gitignore

--- dev/test_ast.ts
+++
@@ -1,1 +0,0 @@
-import { parseToad } from './src/parser/parser.js'; console.dir(parseToad('rect { filter: blur(20px); }', 'test').elements[0].properties, { depth: null });  
--- scratch/test_filter_rule.cjs
+++
@@ -1,6 +0,0 @@
-const { createCanvas, loadImage } = require('@napi-rs/canvas');
-
-(async () => {
-  // Test if Chromium/resvg multiplies or not
-  console.log("Testing filter attributes");
-})();
