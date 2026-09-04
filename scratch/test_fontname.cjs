37ed420c86af3a648e6f7789d3bbff061150896a
razy-me <florian8schuster@gmail.com>
2026-09-04 23:07:54 +0200

    chore: remove scratch/dev folders and legacy request/test planning docs, update .gitignore

--- scratch/test_fontname.cjs
+++
@@ -1,6 +0,0 @@
-const { createCanvas, GlobalFonts } = require('@napi-rs/canvas');
-const c = createCanvas(200, 100);
-const ctx = c.getContext('2d');
-ctx.font = '800 48px sans-serif';
-// Can we find what font was matched?
-console.log('Families count:', GlobalFonts.families.length);
